380fe39d482d921d80c9108234661ab38d9f826e
bluestome <zhangxiao918@21cn.com>
2013-03-17 20:52:34 +0800

    2013-03-17 1。修改AlarmReceiver中onReceive对处理时间过长的函数biz.catalog的异步化，不阻塞主线程逻辑。

--- android/SatelliteWeatherHD/src/org/bluestome/satelliteweather/services/UpdateService.java
+++ android/SatelliteWeatherHD/src/org/bluestome/satelliteweather/services/UpdateService.java
@@ -1,8 +1,5 @@
-package org.bluestome.satelliteweather.services;
 
-import org.bluestome.satelliteweather.MainApp;
-import org.bluestome.satelliteweather.biz.SatelliteWeatherSimpleBiz;
-import org.bluestome.satelliteweather.common.Constants;
+package org.bluestome.satelliteweather.services;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -15,139 +12,145 @@ import android.os.IBinder;
 import android.os.SystemClock;
 import android.text.format.DateUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.bluestome.android.utils.HttpClientUtils;
+
+import org.bluestome.satelliteweather.MainApp;
+import org.bluestome.satelliteweather.biz.SatelliteWeatherSimpleBiz;
+import org.bluestome.satelliteweather.common.Constants;
 
 /**
  * 后台定时更新列表
  * 
  * @author bluestome
- * 
  */
 public class UpdateService extends Service {
 
-	private static String TAG = UpdateService.class.getSimpleName();
+    private static String TAG = UpdateService.class.getSimpleName();
 
-	SatelliteWeatherSimpleBiz biz = null;
-	// 发送统计日志
-	private PendingIntent mSender;
-	private AlarmRecevier mAlarmRecevier;
-	private AlarmManager am;
+    SatelliteWeatherSimpleBiz biz = null;
+    // 发送统计日志
+    private PendingIntent mSender;
+    private AlarmRecevier mAlarmRecevier;
+    private AlarmManager am;
 
-	public UpdateService() {
-		Log.d(TAG, "\tzhang:无参数构造函数");
-	}
+    public UpdateService() {
+        Log.d(TAG, "\tzhang:无参数构造函数");
+    }
 
-	@Override
-	public void onCreate() {
-		Log.d(TAG, "\tzhang:onCreate");
-		super.onCreate();
-		if (null == biz) {
-			biz = new SatelliteWeatherSimpleBiz(null);
-		}
-	}
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "\tzhang:onCreate");
+        super.onCreate();
+        if (null == biz) {
+            biz = new SatelliteWeatherSimpleBiz(null);
+        }
+    }
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.d(TAG, "\tzhang:onStartCommand,flags[" + flags + "],startId["
-				+ startId + "]");
-		if ((flags & START_FLAG_RETRY) == 0) {
-			// TODO 执行当前进程
-			initAlarmRecevier();
-		} else {
-			// TODO 其他替换逻辑
-		}
-		return Service.START_STICKY;
-	}
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "\tzhang:onStartCommand,flags[" + flags + "],startId["
+                + startId + "]");
+        initAlarmRecevier();
+        return Service.START_STICKY;
+    }
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		Log.d(TAG, "\tzhang:onBind");
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent arg0) {
+        Log.d(TAG, "\tzhang:onBind");
+        return null;
+    }
 
-	@Override
-	public void onLowMemory() {
-		Log.d(TAG, "\tzhang:onLowMemory");
-		// TODO 低内存时处理
-		super.onLowMemory();
-	}
+    @Override
+    public void onLowMemory() {
+        Log.d(TAG, "\tzhang:onLowMemory");
+        // TODO 低内存时处理
+        super.onLowMemory();
+    }
 
-	@Override
-	public void onDestroy() {
-		Log.d(TAG, "\tzhang:onDestroy");
-		super.onDestroy();
-		uninitAlarmRecevier();
-		biz = null;
-		Intent serviceIntent = new Intent(this, UpdateService.class);
-		serviceIntent.putExtra("caller", "UpdateService");
-		startService(serviceIntent);
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "\tzhang:onDestroy");
+        super.onDestroy();
+        uninitAlarmRecevier();
+        biz = null;
+        Intent serviceIntent = new Intent(this, UpdateService.class);
+        serviceIntent.putExtra("caller", "UpdateService");
+        startService(serviceIntent);
+    }
 
-	}
+    /**
+     * 初始化设置
+     */
+    public void initAlarmRecevier() {
+        mAlarmRecevier = new AlarmRecevier();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Constants.ACTION_ALARM);
+        registerReceiver(mAlarmRecevier, intentFilter);
+        long firstTime = 0L;
+        am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        if (am != null) {
+            // 发送终端统计数据
+            mSender = PendingIntent.getBroadcast(this, 0, new Intent(
+                    Constants.ACTION_ALARM), 0);
+            firstTime = SystemClock.elapsedRealtime();
+            firstTime += 5 * DateUtils.MINUTE_IN_MILLIS;
+            am.setRepeating(AlarmManager.ELAPSED_REALTIME, firstTime,
+                    5 * DateUtils.MINUTE_IN_MILLIS, mSender);
+        }
+        Log.d(TAG, "\tzhang:initAlarmRecevier");
+    }
 
-	/**
-	 * 初始化设置
-	 */
-	public void initAlarmRecevier() {
-		mAlarmRecevier = new AlarmRecevier();
-		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction(Constants.ACTION_ALARM);
-		registerReceiver(mAlarmRecevier, intentFilter);
-		long firstTime = 0L;
-		am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-		if (am != null) {
-			// 发送终端统计数据
-			mSender = PendingIntent.getBroadcast(this, 0, new Intent(
-					Constants.ACTION_ALARM), 0);
-			firstTime = SystemClock.elapsedRealtime();
-			firstTime += 30 * DateUtils.MINUTE_IN_MILLIS;
-			am.setRepeating(AlarmManager.ELAPSED_REALTIME, firstTime,
-					30 * DateUtils.MINUTE_IN_MILLIS, mSender);
-		}
-		Log.d(TAG, "\tzhang:initAlarmRecevier");
-	}
+    /**
+     * 反注册监听器
+     */
+    public void uninitAlarmRecevier() {
+        if (mAlarmRecevier != null) {
+            unregisterReceiver(mAlarmRecevier);
+        }
+        if (am == null)
+            return;
+        if (mSender != null) {
+            am.cancel(mSender);
+        }
+        Log.d(TAG, "\tzhang:uninitAlarmRecevier");
+    }
 
-	/**
-	 * 反注册监听器
-	 */
-	public void uninitAlarmRecevier() {
-		if (mAlarmRecevier != null) {
-			unregisterReceiver(mAlarmRecevier);
-		}
-		if (am == null)
-			return;
-		if (mSender != null) {
-			am.cancel(mSender);
-		}
-		Log.d(TAG, "\tzhang:uninitAlarmRecevier");
-	}
-
-	/**
-	 * 定时器，每隔半小时访问一次网站
-	 * 
-	 * @author bluestome
-	 * 
-	 */
-	private class AlarmRecevier extends BroadcastReceiver {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			if (intent.getAction().equalsIgnoreCase(Constants.ACTION_ALARM)) {
-				try {
-					String lastModifyTime = HttpClientUtils
-							.getLastModifiedByUrl(Constants.SATELINE_CLOUD_URL);
-					if (null != lastModifyTime
-							&& !lastModifyTime.equals(MainApp.i()
-									.getLastModifyTime())) {
-						biz.notifyNS("服务端有最新数据,"
-								+ com.bluestome.android.utils.DateUtils
-										.getNow());
-						biz.catalog(lastModifyTime);
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    /**
+     * 定时器，每隔半小时访问一次网站
+     * 
+     * @author bluestome
+     */
+    private class AlarmRecevier extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(UpdateService.this, "[SatelliteWeatherHD]启动定时更新任务",
+                    Toast.LENGTH_LONG)
+                    .show();
+            Log.d(TAG, "\tzhang:AlarmReceiver\t:" + intent.getAction());
+            if (intent.getAction().equalsIgnoreCase(Constants.ACTION_ALARM)) {
+                // biz.catalog是一个耗时的任务，需要放到线程中去执行
+                new Thread(new Runnable() {
+                    public void run() {
+                        try {
+                            if (null == biz) {
+                                biz = new SatelliteWeatherSimpleBiz(null);
+                            }
+                            String lastModifyTime = HttpClientUtils
+                                    .getLastModifiedByUrl(Constants.SATELINE_CLOUD_URL);
+                            if (null != lastModifyTime
+                                    && !lastModifyTime.equals(MainApp.i()
+                                            .getLastModifyTime())) {
+                                biz.catalog(lastModifyTime);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        }
+    }
 
 }
